bd7cb032a78cf780d9e919e34bbdb37241334ff3
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-05 13:59:33 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tikchat/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/tikchat/app/MainActivity.kt
@@ -1,5 +1,8 @@
-package com.tikchat.app
+package com.dardashati.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // لا حاجة لإضافة كود إضافي هنا حالياً، 
+    // فلاتر ستتعامل مع كل شيء من خلال هذا التعريف الصغير والمنظم.
+}
